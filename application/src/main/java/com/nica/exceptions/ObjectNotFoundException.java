c00dacec7b51909316cdf4d4117a5df84af4a247
Julio <jherdia@g.educaand.es>
2024-10-24 11:40:12 +0200

    Fix errorCode naming and add getter.

--- application/src/main/java/com/nica/exceptions/ObjectNotFoundException.java
+++ application/src/main/java/com/nica/exceptions/ObjectNotFoundException.java
@@ -1,9 +1,13 @@
 package com.nica.exceptions;
 
 public class ObjectNotFoundException extends RuntimeException{
-    private static final int errorCode = 404;
+    private static final int ERROR_CODE = 404;
 
     public ObjectNotFoundException(String message) {
         super(message);
+    }
+
+    public static int getErrorCode() {
+        return ERROR_CODE;
     }
 }
